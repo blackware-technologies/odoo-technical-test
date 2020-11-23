27f266805013b449fe52983f6961494f09b0c69f
Coco60 <marco_deschamps@hotmail.com>
2020-11-23 17:13:28 -0500

    finished test

--- addons/bwt_sale_internal_reference/models/sale_order.py
+++ addons/bwt_sale_internal_reference/models/sale_order.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    internal_ref = fields.Char(string='Internal Reference')
+    internal_ref = fields.Char(string='Internal Reference', readonly=True)
+
+    user_id = fields.Many2one('res.users', string='User')
+
+    @api.onchange('user_id')
+    def _onchange_user_id(self):
+        self.internal_ref = "Hello " + self.user_id.name
